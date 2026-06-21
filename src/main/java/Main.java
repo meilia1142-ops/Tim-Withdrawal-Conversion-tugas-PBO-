4fdf3a3b18935fa70ce6eb783adda859c4ec9e6e
dewaasubkhasyirozin-dot <dewaasubkhasyirozin@gmail.com>
2026-06-21 10:53:10 +0700

    Implement main method to launch DashboardFrame

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,1 +1,13 @@
-https://lms.umm.ac.id/login/index.php
+import gui.DashboardFrame;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            DashboardFrame frame = new DashboardFrame();
+            frame.setVisible(true);
+        });
+    }
+}
